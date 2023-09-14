027b777d90392428cb744cb6a98aba6cfdaea956
DuyTungTMT <tata.nguyenduytung169@gmail.com>
2023-09-14 23:31:27 +0700

    14-9 #6

--- src/config/routes.js
+++ src/config/routes.js
@@ -1,12 +1,12 @@
 export const routes = {
-    home: '/',
-    release: '/release',
-    odd: '/odd',
-    series: '/series',
-    wishlist: '/wishlist',
-    login: '/login',
-    donate: '/donate',
-    detail: 'detail/:info',
-    ribbon: 'ribbon/:info',
+    home: '/movie_project/',
+    release: '/movie_project/release',
+    odd: '/movie_project/odd',
+    series: '/movie_project/series',
+    wishlist: '/movie_project/wishlist',
+    login: '/movie_project/login',
+    donate: '/movie_project/donate',
+    detail: '/movie_project/detail/:info',
+    ribbon: '/movie_project/ribbon/:info',
     other: '*'
 }
